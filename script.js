99130d82341d08c6aac4855ef9fd95846e278f16
dune123 <96974209+dune123@users.noreply.github.com>
2024-10-30 12:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const makeChange = (c) => {
   // your name here
+	let ans={
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	}
+
+	let coins=[25,10,5,1];
+
+	coins.map((item)=>{
+    let currCoin=0;
+
+		if(item<=c) currCoin=Math.floor(c/item);
+		
+		if(item==25){
+			ans.q=currCoin;
+		}
+		if(item==10){
+			ans.d=currCoin;
+		}
+		if(item==5){
+			ans.n=currCoin;
+		}
+		if(item==1){
+			ans.p=currCoin;
+		}
+		c=c%item;
+	})
+
+	return ans;
 };
 
 // Do not the change the code below
